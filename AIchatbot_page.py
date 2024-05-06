365f60c0e578336a7e46bc5eff5d9afc9472fee8
Yusen Wang <117229722+tonywang0418@users.noreply.github.com>
2024-05-06 13:34:37 -0700

    Update AIchatbot_page.py
     User now won't be able to click login button once they already logged in
     User now won't be able to SEE or Access the content by clicking or typing the URL directly if they are not logged in. (hide content page)
     User now can only see the Sign out button after they authenticate(They used to see the login button too when they logged in)

--- AIchatbot_page.py
+++ AIchatbot_page.py
@@ -1,21 +1,20 @@
 import streamlit as st
 import hashlib
-from streamlit.web.server.websocket_headers import _get_websocket_headers
-# Retrieve the access token from query parameters
-access_token = st.query_params.get("access_token")
+from st_pages import Page, show_pages, hide_pages
+redirect_login_url = "http://localhost:8501/login%20page"
+st.title("AI Chatbot Page")
+st.write(st.session_state)
 
-if not access_token:
-    st.title("Blocked")
-    st.write("Access token:", access_token)
-    st.error("No access token provided")
-    redirect_login_url = "<PORT TO THE LOGIN PAGE>"
+if not st.session_state:
+    st.write("BLOCKED")
     st.markdown(f'<meta http-equiv="refresh" content="0;URL={redirect_login_url}">', unsafe_allow_html=True)
     st.stop()
 else:
-    st.title("AI Chatbot Page")
-    # Store the access token in session state
-    st.session_state.access_token = access_token
-    st.write(st.session_state)
+    show_pages([Page("AIchatbot_page.py", "AIchatbot page"), Page("login_page.py", "login page")])
+    hide_pages(["login page"])
+    if st.button("Sign out"):
+        st.session_state.token = None
+        st.markdown(f'<meta http-equiv="refresh" content="0;URL={redirect_login_url}">', unsafe_allow_html=True)
 
     
     
