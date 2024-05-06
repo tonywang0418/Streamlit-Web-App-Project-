a03b9e1776cd9fa09ef7088df29159c282a720a0
Yusen Wang <117229722+tonywang0418@users.noreply.github.com>
2024-05-06 13:36:08 -0700

    Update login_page.py
    Update to version 2
    Fixed few bugs

--- login_page.py
+++ login_page.py
@@ -1,20 +1,17 @@
 import streamlit as st
 from msal import PublicClientApplication
 import hashlib
-#import requests
-LOCAL_URL_for_Content = "<LOCAL URL FOR YOUR APP CONTENT>"
+from st_pages import Page, show_pages, hide_pages
 # Initialize MSAL PublicClientApplication
 app = PublicClientApplication(
-    "<CLIENT ID>",
+    "<Client ID>",
     authority="https://login.microsoftonline.com/<Tenant ID>",
     client_credential=None
     )
-result = None
+
 # Function to acquire and use token
 def acquire_and_use_token():
-    #result = None
-
-    # Attempt to get token from cache or acquire interactively
+    global result
     accounts = app.get_accounts()
     if accounts:
         result = app.acquire_token_silent(["User.Read"], account=accounts[0])
@@ -24,41 +21,45 @@ def acquire_and_use_token():
         st.write("Authenticate to access protected content")
     # Check if token was obtained successfully
     if "access_token" in result:
+        st.write(result)
         st.session_state.token = result["access_token"]
         st.write("Protected content available")
-        hashed_token = hashlib.sha256(st.session_state.token.encode()).hexdigest()
-        second_app_url_with_token = f"{LOCAL_URL_for_Content}/?access_token={hashed_token}"
-        st.write(f'<iframe src="{second_app_url_with_token}" width="800" height="800"></iframe>', unsafe_allow_html=True)
+        st.rerun()
     else:
         st.error("Token acquisition failed")
         st.error(result.get("error_description", "No further details"))
-    
+
 def logout():
     logged_accounts = app.get_accounts()
     if logged_accounts:
         for account in logged_accounts:
             app.remove_account(account)
         st.session_state.token = None
-            
-    
-# Streamlit app UI
-st.title("Azure Entra ID Login Page")
+
 
 # Create a placeholder for token
 if "token" not in st.session_state:
     st.session_state.token = None
-
+    
 add_selectbox = st.sidebar.container(border=True)
-if add_selectbox.button("login", type= "primary"):
-    acquire_and_use_token()
-    # Update session state with token
-
+if not st.session_state.token:
+    login_button_clicked = add_selectbox.button("Login", type="primary")
+    if login_button_clicked:
+        acquire_and_use_token()
+        
+        
 if add_selectbox.button("Sign out"):
     st.session_state.token = None
     logout()
-
-if not result:
+    st.rerun()
+if not st.session_state.token:
+    st.title("Azure Entra ID Login Page")
     st.write("Authenticate to access protected content")
+    show_pages([Page("AIchatbot_page.py", "AIchatbot page"), Page("login_page.py", "login page")])
+    hide_pages(["AIchatbot page"])
+else:
+    st.title("Welcome! AI Chatbot ")
+    
 
 st.write(st.session_state)
 
