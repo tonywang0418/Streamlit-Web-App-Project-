82c091a1456cf08f3e1e52411f0d34ce01714d76
Yusen Wang <117229722+tonywang0418@users.noreply.github.com>
2024-05-18 10:15:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import streamlit as st
-from login_page import acquire_and_use_token
+import hashlib
+import pymysql
+from login_page import acquire_and_use_token, show_dialog
 from AIchatbot_page import main_content
-
-
+#st.session_state.login_state = None
 def main():
-    add_selectbox = st.sidebar.container(border=True)
-    st.write(st.session_state)
+    #add_selectbox = st.sidebar.container(border=True)
 
-    if "access_token" not in st.session_state or st.session_state.access_token == None:
-        
-        st.title("Azure Entra ID Login Page")
+    st.write(st.session_state)
+    
+    #if "login_state" not in st.session_state and "access_token" not in st.session_state:
+    if 'login_state' not in st.session_state:
+        st.session_state.login_state = None
+    if 'access_token' not in st.session_state:
+        st.session_state.access_token = None
+    if 'password_requirement' not in st.session_state:
+        st.session_state.password_requirement = None
+    if st.session_state.login_state == None and st.session_state.access_token == None:
+        st.title("Login Page")
         st.write("Authenticate to access protected content")
+        # Below is for Azure Entra ID LOGIN
         add_selectbox = st.sidebar.container(border=True)
         login_button_clicked = add_selectbox.button("Azure Login", type="primary")
         if login_button_clicked:
@@ -18,12 +27,21 @@ def main():
             access_token = acquire_and_use_token()
             st.session_state.access_token = access_token
             st.rerun()
+        # Below is for username/password LOGIN
+        if add_selectbox.button("Username/Password Login"):
+            show_dialog()
+            if st.session_state.login_state == True:
+                st.rerun()
+                    
+                
+
     else:
         st.title("AI Chatbot Page")
         main_content()
-        st.write(st.session_state)
+        #st.write(st.session_state)
         if st.button("Sign out"):
             st.session_state.access_token = None
+            st.session_state.login_state = None
             st.session_state.clear()
             st.rerun()
 
