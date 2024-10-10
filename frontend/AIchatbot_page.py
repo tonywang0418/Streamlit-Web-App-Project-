52319892d90ad734ce7746c8b939ad43c3416a01
Tony <wangyusen2012@gmail.com>
2024-10-09 18:40:31 -0700

    changes

---
+++ frontend/AIchatbot_page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+#import hashlib
+#from st_pages import Page, show_pages, hide_pages
+
+def main_content():
+   
+    st.title("Content right here")
+    st.image("ds.png")
+    
